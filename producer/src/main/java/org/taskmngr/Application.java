3f5fbd1a9c89559e9a87f1dd09339c4a1e136ba9
Alexandr Hapon <full8back@gmail.com>
2024-12-14 01:22:19 +0300

    first commit

--- producer/src/main/java/org/taskmngr/Application.java
+++ producer/src/main/java/org/taskmngr/Application.java
@@ -1,11 +1,14 @@
 package org.taskmngr;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class Application {
     public static void main(String[] args) {
+        log.info("higit ");
         SpringApplication.run(Application.class,args);
         System.out.println("Hello world from Prod!");
     }
