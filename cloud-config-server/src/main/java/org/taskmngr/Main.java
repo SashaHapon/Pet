9bd3cda8a3a28959cec654739c5db9d6643922fd
Alexandr Hapon <full8back@gmail.com>
2024-12-14 19:34:01 +0300

    git-cloud-repository works

---
+++ cloud-config-server/src/main/java/org/taskmngr/Main.java
@@ -0,0 +1,13 @@
+gpackage org.taskmngr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class Main {
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class);
+    }
+}
